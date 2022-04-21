e7f8ffe17d0beff24d7f9960f471402c6e4d6d4e
aleexjimeenz1 <aleexjimeenz@gmail.com>
2022-04-21 10:18:00 +0200

    noi
    yigoi

---
+++ src/Ejercicio7/Cinco.java
@@ -0,0 +1,38 @@
+package Ejercicio7;
+/**
+ * 
+ * @author Alejandro Jimenez Lara
+ * @version 1.0
+ *
+ */
+public class Cinco {
+	
+	private String git;
+	
+	/**
+	 * 
+	 * @param args
+	 * @return cuantas canastas has metido
+	 * 
+	 * @see <a href="https://www.google.es/">Es un link </a>
+	 * @since 1.0
+	 *  
+	 */
+	
+	private int tiros(int canastas) {
+		return 0;
+	}
+	
+	public static void main(String [ ]args) {
+		
+		/**
+		 * @param x
+		 * @param y
+		 */
+		
+		int x=1;
+		int y=2;
+		
+		System.out.println(x+y);
+	}
+}
